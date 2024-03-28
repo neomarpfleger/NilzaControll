2942231dd45b493de7ac4907484bae06df7dc728
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2024-03-28 06:47:55 -0300

    28032024
    Adicionado a  funcionalidade do historicoEPI.js aparentemente tudo ok.

---
+++ js/historicoEPI.js
@@ -0,0 +1,30 @@
+async function listaHistorico() {
+    const conexao = await fetch("http://localhost:3000/uniformeEPI");
+    const conexaoConvertida = await conexao.json();
+
+    console.log("Lista de EPIs:", conexaoConvertida);
+
+    const listaDeHistorico = document.querySelector(".listaDeHistorico");
+
+    conexaoConvertida.forEach(epi => {
+        const item = constroiItem(epi.nome, epi.uniformeEPI, epi.tamanho, epi.data, epi.dataDeEntrega);
+        listaDeHistorico.appendChild(item);
+    });
+
+    return conexaoConvertida;
+}
+
+function constroiItem(nome, uniformeEPI, tamanho, data, dataDeEntrega) {
+    const listaDeHistorico = document.createElement("li");
+    listaDeHistorico.className = "pedidosEntregues";
+    listaDeHistorico.innerHTML =
+        `<p class="nome">${nome}</p>
+        <p class="uniformeEPI">${uniformeEPI}</p>
+        <p class="tamanho">${tamanho}</p>
+        <p class="data">${data}</p>
+        <p class="data">${dataDeEntrega}</p>`;
+
+    return listaDeHistorico;
+}
+
+listaHistorico();
