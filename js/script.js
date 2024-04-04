979d319064e3b3fad2452d2ec4099cb3d96c0084
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2024-04-04 04:55:50 -0300

    04042024
    ajuste para o banco do cloud firestore.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let nomeUsuarioLogado; // Variável global para armazenar o nome do usuário logado
+/*let nomeUsuarioLogado; // Variável global para armazenar o nome do usuário logado
 
 async function verificarLogin(nome, senha) {
     const conexao = await fetch("http://localhost:3000/usuario");
@@ -15,6 +15,57 @@ async function verificarLogin(nome, senha) {
 
     return false; // Retorna false se o usuário não for válido
 }
+
+btnLogin.addEventListener("click", async function(){
+    const nome = document.getElementById("nome").value;
+    const senha = document.getElementById("senha").value;
+
+    const usuarioValido = await verificarLogin(nome, senha);
+
+    if (!usuarioValido) {
+        alert("Nome de usuário ou senha incorretos. Por favor, tente novamente.");
+    }
+});*/
+
+
+let nomeUsuarioLogado; // Variável global para armazenar o nome do usuário logado
+
+async function verificarLogin(nome, senha) {
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyBKmHpwQWG0vCHael2iMFYzgNv_2ctlij4",
+        authDomain: "nilza-controll.firebaseapp.com",
+        projectId: "nilza-controll",
+        storageBucket: "nilza-controll.appspot.com",
+        messagingSenderId: "304218993931",
+        appId: "1:304218993931:web:56ce1292e315fbfbccde0e",
+        measurementId: "G-TM623T80R8"
+    };
+    
+    // Inicializa o Firebase
+    firebase.initializeApp(firebaseConfig);
+    
+    var db = firebase.firestore();
+    var usersRef = db.collection('categorias');
+    
+    // Consulta no Firestore para verificar se o usuário e senha estão corretos
+    const snapshot = await usersRef.where('nome', '==', nome).where('senha', '==', senha).get();
+
+    if (snapshot.empty) {
+        return false; // Retorna false se não houver nenhum documento que corresponda ao nome e senha
+    }
+
+    snapshot.forEach((doc) => {
+        nomeUsuarioLogado = nome; // Salva o nome do usuário na variável global
+        localStorage.setItem('nomeUsuarioLogado', nome); // Salva o nome do usuário no localStorage
+        window.location.href = `./html/solicitacaoEPI.html?nome=${nome}`;
+    });
+
+    return true; // Retorna true se o usuário for válido
+}
+
+// Adicionei a referência para o botão de login
+let btnLogin = document.getElementById("btnLogin");
 
 btnLogin.addEventListener("click", async function(){
     const nome = document.getElementById("nome").value;
